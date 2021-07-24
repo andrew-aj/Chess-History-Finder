9e938ce5d73eb8dfe5c1ac6ce8d8372e39c5d4c8
joey <asleeponpluto@gmail.com>
2021-07-24 13:40:19 -0400

    added color of which side plays to the NextMove struct

--- NextMove.h
+++ NextMove.h
@@ -1,14 +1,12 @@
-
-
-#ifndef CHESSHISTORYFINDER_NEXTMOVE_H
-#define CHESSHISTORYFINDER_NEXTMOVE_H
+#pragma once
 
 namespace Chess {
 
     struct NextMove {
         //Store in short using 14 bits.
-        //Format: 0011111111111111
-        //First 2 bits are empty.
+        //Format: 0111111111111111
+        //First bit is empty.
+        //Next bit represents whether move is white (0) or black (1)
         //Next 6 bits represent from tile
         //Next 6 bits represent to tile.
         //Last 2 bits represent promotion if a pawn is moving to the opposite end of the board.
@@ -18,6 +16,3 @@ namespace Chess {
     };
 
 }
-
-
-#endif //CHESSHISTORYFINDER_NEXTMOVE_H
