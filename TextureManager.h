d1f3f14f49612ef522204603fefc0b62ab3873a0
joey <asleeponpluto@gmail.com>
2021-07-29 00:05:21 -0400

    cleaned up texture manager, we'll load in files with an iterative for loop for the pieces and manually for the other components

--- TextureManager.h
+++ TextureManager.h
@@ -1,35 +1,16 @@
-//
-// Created by hicke on 7/26/2021.
-//
-
-#ifndef CHESSHISTORYFINDER_TEXTUREMANAGER_H
-#define CHESSHISTORYFINDER_TEXTUREMANAGER_H
-
+#pragma once
+#include <SFML/Graphics.hpp>
 #include <unordered_map>
 #include <string>
-#include <iostream>
-#include <filesystem>
-#include <SFML/Graphics.hpp>
-
-using std::string;
 using std::unordered_map;
-using std::cout;
-using std::endl;
-using std::filesystem::directory_iterator;
+using std::string;
 
-namespace Chess{
-    class TextureManager{
+namespace Chess {
+    class TextureManager {
         static unordered_map<string, sf::Texture> textures;
         static void LoadTexture(string textureName);
-        //static string imageDirectory;
-
     public:
-        static sf::Texture& GetTexture(string TextureName);
-        static void Clear(); //call me at the end of main
-        static void LoadImages();
-
+        static sf::Texture& GetTexture(string textureName);
+        static void Clear();
     };
 }
-
-
-#endif //CHESSHISTORYFINDER_TEXTUREMANAGER_H
