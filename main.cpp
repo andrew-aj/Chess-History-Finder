6274cf3fa022f90884c20a286ffe0c1affb3de52
andrew-aj <andrew.knee.j@gmail.com>
2021-08-02 00:26:50 -0400

    new fancy main

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 #include "PgnPreprocessor.h"
 #include "ZobristHash.h"
 #include "Heap.h"
+
 using Chess::ZobristHash;
 
-int main() {
+int main(int argc, const char *argv[]) {
 
     /* Code to start gui */
-    Chess::GUI gui;
-    gui.readFileBinary("outbin.effective");
-    gui.startProgramWindow();
+    if (argc == 2) {
+        Chess::GUI gui;
+        gui.readFileBinary(argv[1]);
+        gui.startProgramWindow();
+    } else if (argc == 3) {
+        /* Code to start preprocessor */
+        Chess::ZobristHash::generateRandomNumbers();
+        Chess::PgnPreprocessor preprocessor;
+        preprocessor.readFile(argv[1]);
+        preprocessor.calculateNextBestMoves();
+        preprocessor.writeFileBinary(argv[2]);
+    }
 
-    /* Code to start preprocessor */
-//    Chess::ZobristHash::generateRandomNumbers();
-//    Chess::PgnPreprocessor preprocessor;
-//    preprocessor.readFile("test_small.pgn");
-//    preprocessor.calculateNextBestMoves();
-//    preprocessor.writeFileBinary("outbin.effective");
 
 }
